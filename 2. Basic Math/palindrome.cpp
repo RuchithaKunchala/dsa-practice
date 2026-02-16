a5e8d0b6ffb05139303ef85a3839465828820589
Ruchitha Kunchala <60230072+RuchithaKunchala@users.noreply.github.com>
2026-02-16 20:01:51 +0530

    Create palindrome.cpp

---
+++ 2. Basic Math/palindrome.cpp
@@ -0,0 +1,14 @@
+//This solution has best time and space complexity.
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0) ) return false; 
+        int rev=0;  
+        while(x>rev) { 
+            rev=10*rev + (x%10); 
+            x=x/10; 
+        } 
+        if(rev==x || rev/10==x) return true; 
+        else return false; 
+    } 
+}; 
