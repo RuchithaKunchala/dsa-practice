acf50eb971450f18da91046ccf3000925f4bf68f
Ruchitha Kunchala <60230072+RuchithaKunchala@users.noreply.github.com>
2026-02-16 21:46:49 +0530

    Update count-digits.cpp

--- 2. Basic Math/LC-2520-count-digits.cpp
+++ 2. Basic Math/LC-2520-count-digits.cpp
@@ -4,9 +4,9 @@ Link: https://leetcode.com/problems/count-the-digits-that-divide-a-number/descri
 Platform: LeetCode 
 Difficulty: Easy 
 Approach: Basic math  
-Time: O(log n) 
+Time: O(log n) or O(d); d = digits in x
 Space: O(1)  
-*/
+*/ 
 class Solution {
 public:
     int countDigits(int num) {
@@ -14,9 +14,8 @@ public:
         int digit=num;
         while(num>0) {
             int last=num%10;
-            if(digit%last == 0) {
+            if(last!=0 && digit%last == 0) //Division by zero leads to runtime crash 
                 count++;
-            }
             num=num/10;
         } 
         return count;
