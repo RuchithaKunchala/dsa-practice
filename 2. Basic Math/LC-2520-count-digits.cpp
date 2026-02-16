7804c804859534d1bb82231dbeb65024783ba3b5
Ruchitha Kunchala <60230072+RuchithaKunchala@users.noreply.github.com>
2026-02-16 20:42:02 +0530

    Create LC-2520-count-digits.cpp

---
+++ 2. Basic Math/LC-2520-count-digits.cpp
@@ -0,0 +1,24 @@
+/*
+Problem: Count the Digits that Divide a number
+Link: https://leetcode.com/problems/count-the-digits-that-divide-a-number/description/
+Platform: LeetCode
+Difficulty: Easy
+Approach: Basic math 
+Time: O(log n) 
+Space: O(1)  
+*/
+class Solution {
+public:
+    int countDigits(int num) {
+        int count=0;
+        int digit=num;
+        while(num>0) {
+            int last=num%10;
+            if(digit%last == 0) {
+                count++;
+            }
+            num=num/10;
+        } 
+        return count;
+    } 
+}; 
